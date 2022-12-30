c64133f9b678c154647228006c7bd09019301ab2
Tim Van Wassenhove <tim@timvw.be>
2022-12-30 07:38:57 +0100

    added rust/tauri src

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,45 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::sync::Arc;
+use datafusion::arrow::array::Array;
+use datafusion::arrow::datatypes::DataType;
+use datafusion::arrow::error::ArrowError;
+use datafusion::error::DataFusionError;
+use datafusion::prelude::*;
+use serde_json::{json, Map, Value};
+
+fn dfe_to_s(e: DataFusionError) -> String {
+    format!("df error: {:?}", e)
+}
+
+fn ae_to_s(e: ArrowError) -> String {
+    format!("ae error: {:?}", e)
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+async fn greet(name: &str, state: tauri::State<'_, Arc<SessionContext>>) -> Result<Vec<Map<String, Value>>, String> {
+    let df = state.sql(name).await.map_err(dfe_to_s)?;
+    let results = df.collect().await.map_err(dfe_to_s)?;
+    //datafusion::arrow::util::pretty::print_batches(&results);
+    //Ok(format!("Hello, {}! You've been greeted from Rust!", name))
+    //let value = json!({ "an": "object" });
+
+    let value = datafusion::arrow::json::writer::record_batches_to_json_rows(&results).map_err(ae_to_s)?;
+    Ok(value)
+}
+
+fn main() {
+
+    let config = SessionConfig::new().with_information_schema(true);
+    let ctx = SessionContext::with_config(config);
+
+    tauri::Builder::default()
+        .manage(Arc::new(ctx))
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
